ac7eb55c082e1fd417041c450ae18bb71e901a08
Your Name <ovifeanyichukwu@gmail.com>
2022-10-14 10:18:19 -0700

    n

---
+++ 0x00-hello_world/101-quote.v
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - starting point of all C programs
+* Return: returns an integer
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+/*having fun*/
