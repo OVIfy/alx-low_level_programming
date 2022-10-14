631627960e94bd42895e63865988131ec5efe5eb
Your Name <ovifeanyichukwu@gmail.com>
2022-10-14 08:35:03 -0700

    yeah

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main(void)
 {
-	char msg[] = "Programming is like buildin a multilingual puzzle";
+	char msg[] = "Programming is like building a multilingual puzzle";
 	puts(msg);
 	return(0);
 }
