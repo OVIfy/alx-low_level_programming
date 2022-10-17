f445cb1e604b1e2e954df7ee7c64a584062e1337
Your Name <ovifeanyichukwu@gmail.com>
2022-10-17 05:03:19 -0700

    g

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+/**
+* main - this function is the entry function for all C programs
+* Return: it returns an integer
+*/
+int main(void)
+{
+	int num  = 49;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num + '\0');
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
