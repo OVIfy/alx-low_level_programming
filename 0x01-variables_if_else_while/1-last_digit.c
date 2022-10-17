0387e8acb2e61fd1cae7501fb1145e7bde3eda49
Your Name <ovifeanyichukwu@gmail.com>
2022-10-16 18:16:42 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 /**
 * main - this function is the entry function for all C programs
 * Return: it returns an integer
 */
 int main(void)
 {
-	if(n > 0)
-	printf("%d is positive\n",n);
-	else
-	printf("%d is negative\n",n);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int l = n % 10;
+
+	if (l > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n,l);
+	else if (l == 0)
+	printf("Last digit of %d is %d and is 0\n", n,l);
+	else if (l < 6 && l != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
+
+	return (0);
 }
