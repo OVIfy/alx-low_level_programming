bd87bdc7e51af476c5b26f182557564bad0264f5
Your Name <ovifeanyichukwu@gmail.com>
2022-10-16 17:33:55 -0700

    wahala no dey finish

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - this function is the entry function for all C programs
+* Return: it returns an integer
+*/
+int main(void)
+{
+	if(n > 0)
+	printf("%d is positive\n",n);
+	else
+	printf("%d is negative\n",n);
+}
