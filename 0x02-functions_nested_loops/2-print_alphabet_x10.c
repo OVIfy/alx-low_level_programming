e02d512e68d9a277894ac814f239127149ccea16
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 03:15:48 -0700

    coulda gone short

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - entry point of all C programms
+* Return: returns an integer
+*/
+int main(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		putchar(ch);
+		if (ch == 'z')
+		{
+			putchar('\n');
+			ch = 'a';
+			i++;
+			putchar(ch);
+		}
+		if(i == 10)
+			break;
+	}
+	return (0);
+}
