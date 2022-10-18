401f029b2c864aa03d0d46892e08a91dbef08d22
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 02:54:36 -0700

    yesterday

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point of all C programms
+* Return: returns an integer
+*/
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		if (ch == 'z')
+			putchar('\n');
+	}
+	return (0);
+}
