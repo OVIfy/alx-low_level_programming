92a564c0cf22b2dfe202673a7d9a66fdb2fb6b62
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 15:16:18 -0700

    h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,12 @@ void print_alphabet_x10(void)
 	char ch = 'a';
         int i = 0;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
+        for (i = 0; i <= 9; i++)
         {
-                _putchar(ch);
-                if (ch == 'z')
-                {
-                        _putchar('\n');
-                        ch = 'a';
-                        i++;
-                        _putchar(ch);
-                }
-                if (i == 10)
-                        break;
-        }
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
 }
