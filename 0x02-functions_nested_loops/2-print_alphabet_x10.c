413698879de29a666582ed73359066dbb113a326
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 15:21:35 -0700

    h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*
-*
+* print_alphabet_x10 - prints alphabet 10 times
+* Return: returns nothing
 */
 void print_alphabet_x10(void)
 {
 	char ch = 'a';
-        int i = 0;
-
-        for (i = 0; i <= 9; i++)
-        {
+	int i = 0;
+	
+	for (i = 0; i <= 9; i++)
+	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
