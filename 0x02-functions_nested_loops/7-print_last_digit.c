67556b4995f63634001cd21e3441ea9fd18d57c8
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 16:25:48 -0700

    g

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* _abs - for returning absolut value of a number re
+* @num: number to be absoluted
+* Return: absoluted integert
+*/
+int print_last_digit(int num)
+{
+	int l;
+
+	l = num % 10;
+	return (l);
+}
