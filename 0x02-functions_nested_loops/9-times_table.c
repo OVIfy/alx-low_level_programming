9128813f5784c1ee9439a43c220e2e37961d68b6
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 16:56:04 -0700

    y

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* times_table - ks if argument is an alphabet
+* Return: 1 if number is a small letter , 0 if it isn't
+*/
+int times_table()
+{
+	int i = 0;
+	int j = 0;
+	int m = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			m = i * j;
+			_putchar(m + '0');
+			_putchar(',');
+			_purchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	for (i = 00; i <= 20; i++)
+	{
+		printf("%u",i);
+	{
+	return (0);
+}
