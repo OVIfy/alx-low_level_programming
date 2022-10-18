c0f34aaa7f724d2b64668291ddc25ba26a4f0d11
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 14:36:07 -0700

    y

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+*
+*
+*/
+void print_alphabet_x10(void)
 {
-	return (write(1, &c, 1));
-}
+	char ch = 'a';
+        int i = 0;
 
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                _putchar(ch);
+                if (ch == 'z')
+                {
+                        _putchar('\n');
+                        ch = 'a';
+                        i++;
+                        _putchar(ch);
+                }
+                if (i == 10)
+                        break;
+        }
+}
 /**
 * main - entry point of all C programms
 * Return: returns an integer
 */
 int main(void)
 {
-	char ch = 'a';
-	int i = 0;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-		if (ch == 'z')
-		{
-			_putchar('\n');
-			ch = 'a';
-			i++;
-			_putchar(ch);
-		}
-		if (i == 10)
-			break;
-	}
+	print_alphabet_x10();
 	return (0);
 }
