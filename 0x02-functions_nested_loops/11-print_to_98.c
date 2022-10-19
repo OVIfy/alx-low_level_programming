e4019f3a0b2bf9927d9760672cdad6f70c089c3d
Your Name <ovifeanyichukwu@gmail.com>
2022-10-18 18:02:00 -0700

    k

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* print_to_98 - for returning absolut value of a number re
+* @start: number to start at
+* Return: absoluted integert
+*/
+void print_to_98(int start)
+{
+	int i = 0;
+	
+	for (i = start; i <= 98; i++)
+		printf("%u, ",i);
+	return;
+
+}
