5622005a5b4175aac09ee409dd44c9e1d1abf070
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 16:39:14 -0700

    h

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* print_triangle - checks if input is a digit
+* @n: number to check
+* Return: returns 1 if c is digit else 0
+*/
+
+void print_triangle(int n)
+{
+	int i = 0;
+	int j = 0;
+	int b = n; 
+
+	if (n > 0)
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if ( j >= b )
+					_putchar('#');
+				else
+					_putchar(' ');	
+			}
+			
+			b--;
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
