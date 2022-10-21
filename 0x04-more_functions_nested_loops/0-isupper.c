89515fb9d613e5748b5da154b9b008c719ddf88f
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 13:49:03 -0700

    h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _isdigit - checks if input is a digit
+* @c: number to check
+* Return: returns 1 if c is digit else 0
+*/
+
+int _isdigit(int c)
+{
+	int A = (char)c;
+
+	if (A >= 65 && A <= 90)
+		return (1);
+	else
+		return (0);
+}
