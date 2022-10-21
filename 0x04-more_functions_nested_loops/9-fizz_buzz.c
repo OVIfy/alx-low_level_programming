945dc887162da4d6f2f874eac66f09d9f04eb08d
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 16:12:40 -0700

    hjs

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - entry point of all C programs
+* Return: nothing 
+*/
+int main(void)
+{
+	int i = 1;
+
+	while(i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if(i % 3 == 0)
+			printf("Fizz");
+		else if ( i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%u",i);
+
+		if (i != 100)
+	 		printf(" ");
+		else
+			putchar('\n');	
+		i++;
+	}
+	
+	return (0);
+}
