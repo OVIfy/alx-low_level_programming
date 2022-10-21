0073dab6d59bd914331c9535da2b8b564b1c9681
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 14:16:26 -0700

    j

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - checks if input is a digit
+* Return: returns 1 if c is digit else 0
+*/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
