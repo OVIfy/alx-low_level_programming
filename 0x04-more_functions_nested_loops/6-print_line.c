6a216cd9091a2293e6a246fec36d9ea16c841e47
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 14:59:40 -0700

    d

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_line - checks if input is a digit
+* @n: number to check
+* Return: returns 1 if c is digit else 0
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('_');
+	else
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+	_putchar('\n');
+}
