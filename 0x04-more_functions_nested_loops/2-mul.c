6fdb187e00e47ef3149add043747c51678913b82
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 14:10:37 -0700

    j

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - checks if input is a digit
+* @a: number to check
+* @b: the other num to multiply
+* Return: returns 1 if c is digit else 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
