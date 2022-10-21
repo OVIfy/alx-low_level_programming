5114c3bd3f7c55e63e5551e48737b3e890ef9aeb
Your Name <ovifeanyichukwu@gmail.com>
2022-10-21 14:36:06 -0700

    y

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* more_numbers - checks if input is a digit
+* Return: returns 1 if c is digit else 0
+*/
+
+void more_numbers(void)
+{
+	int i = 0;
+	
+	while (++i <= 10)
+	{
+	for (i = '0'; i <= '14'; i++)
+		_putchar(i);
+	_putchar('\n');
+	}
+}
